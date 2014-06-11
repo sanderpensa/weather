a57cacdb0ee6d5180216aae23626960f0704f9ac
Priit Kallas <kallaspriit@gmail.com>
2014-06-11 14:36:22 +0300

    Implemented dynamic day content.

--- js/weather-app.js
+++ js/weather-app.js
@@ -4,32 +4,35 @@
 
 
 $(document).ready(function() {
-	var previousIndex = 10;
-	//display content when day-tab is clicked
+	// opens day tab, closing existing one
 	$('.day-tab').click(function() {
-		var headerIndex = $('.day-tab').index(this);
-		console.log('This content has index ' + headerIndex + ' previous index = ' + previousIndex);
-
-		if (previousIndex !== headerIndex){
-			$('.item').eq(headerIndex).addClass('active').siblings().removeClass('active');
-			previousIndex = headerIndex;
+		if ($(this).parent().hasClass('active')) {
+			$(this).parent().removeClass('active');
 		} else {
-			$('.item').eq(headerIndex).removeClass('active');
-			previousIndex = 10;
+			$('.item.active').removeClass('active');
+			$(this).parent().addClass('active');
 		}
 	});
 
+	// remove active class from active item
 	$('.weather-temp').click(function() {
-		$('.item').removeClass('active');
+		$('.item.active').removeClass('active');
 	});
 
-	//set secondary-content background to primary-content background
-	$('ul li').click(function() {
-		var contentColor = $(this).css('background-color');
+	// set secondary-content background to primary-content background
+	$('.period-choice li').click(function() {
+		var contentColor = $(this).css('background-color'),
+			tabName = $(this).data('tab');
+
+		// hide currently active tab
+		$(this).parent().parent().find('.secondary-content .tab-pane.is-active').removeClass('is-active');
+
+		// show selected tab
+		$(this).parent().parent().find('.secondary-content .tab-pane[data-tab="' + tabName + '"]')
+			.addClass('is-active');
+
+		console.log('open tab', tabName);
 
 		$('.content .secondary-content').css('background-color', contentColor);
-
 	});
-
-
 });
