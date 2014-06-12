6e895eccce822e14a455ab8a162bbc15097f2683
Priit Kallas <kallaspriit@gmail.com>
2014-06-12 13:13:41 +0300

    Implemented basic day rendering.

--- js/Ui.js
+++ js/Ui.js
@@ -8,31 +8,26 @@
 
 	};
 
-	Ui.prototype.className = {
-		ITEM: 'item',
-		CLEARFIX: 'clearfix',
-		DAY_TAB: 'day-tab',
-		SECONDARY_CONTENT: 'secondary-content',
-		WEATHER_ICON: 'weather-icon',
-		TEMP_HIGH: 'temp-high',
-		TEMP_LOW: 'temp-low',
-		CONTENT: 'content',
-		PRIMARY_CONTENT: 'primary-content',
-		PERIOD_CHOICE: 'period-choice',
-		TITLE: 'title',
-		TAB_PANE: 'tab-pane',
-		IS_ACTIVE: 'is-active',
-		LOCATION: 'location',
-		WEATHER_TEMP: 'weather-temp'
-	};
-
 	Ui.prototype._renderWeatherDay = function(weatherDay) {
-		$('#weather').html($('<article>', {
-			class: this.className.ITEM
-		}));
-		$('.item').html($('<header>', {
-			class: this.className.CLEARFIX + ' ' +  this.className.DAY_TAB
-		}));
+		var wrap = $('#weather'),
+			article = $('<article>', {
+				className: 'item'
+			}),
+			/*header = $('<header>', {
+				className: 'secondary-content clearfix'
+			}).appendTo(article);*/
+			header = $(
+				'<header class="clearfix day-tab">' +
+				'<div class="secondary-content clearfix">' +
+				'	<span class="weather-icon"></span>' +
+				'	<p class="temp-high">29&deg;<br><small>hi</small></p>' +
+				'	<p class="temp-low">16&deg;<br><small>lo</small></p>' +
+				'</div>' +
+				'<h1>Monday</h1>' +
+				'</header>'
+			).appendTo(article);
+
+		wrap.append(article);
 	};
 
 	exports.Ui = Ui;
